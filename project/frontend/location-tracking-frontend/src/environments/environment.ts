67b036349e843b7dff576a5ef6544760ca791f6d
FilipeMCruz <1170911@isep.ipp.pt>
2021-11-15 18:25:14 +0000

    Fixed some css style problems and changed the default map style

--- project/frontend/location-tracking-frontend/src/environments/environment.ts
+++ project/frontend/location-tracking-frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
   },
   mapbox: {
     accessToken: "pk.eyJ1IjoiZmlsaXBlY3J1eiIsImEiOiJja215bGkxZWcwNTJqMndwa3pueGdjeDhzIn0.L0aoXWF42aUw8RgRWBKnYA",
-    style: "mapbox://styles/mapbox/streets-v11"
+    style: "mapbox://styles/mapbox/light-v10"
   }
 };

45fb7b962acdc3432599f7af0fa601752bdcdd3b
FilipeMCruz <1170911@isep.ipp.pt>
2021-12-11 14:36:49 +0000

    Fixed bug related to websocket connection being completed without it being suposed to

--- project/backend-services/sharespot-location-tracking-backend/application/src/main/java/sharespot/services/locationtrackingbackend/application/publishers/GPSDataPublisher.java
+++ project/backend-services/sharespot-location-tracking-backend/application/src/main/java/sharespot/services/locationtrackingbackend/application/publishers/GPSDataPublisher.java
@@ -2,29 +2,36 @@ package sharespot.services.locationtrackingbackend.application.publishers;
 
 import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Sinks;
-import sharespot.services.locationtrackingbackend.application.exceptions.PublishErrorException;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import sharespot.services.locationtrackingbackend.domain.sensor.gps.RecordEntry;
 import sharespot.services.locationtrackingbackend.domain.sensor.gps.SensorData;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class GPSDataPublisher {
 
-    private final Sinks.Many<SensorData> sink;
+    private FluxSink<SensorData> dataStream;
+    private ConnectableFlux<SensorData> dataPublisher;
 
-    public GPSDataPublisher() {
-        this.sink = Sinks
-                .many()
-                .multicast()
-                .onBackpressureBuffer();
+    @PostConstruct
+    public void init() {
+        Flux<SensorData> publisher = Flux.create(emitter -> {
+            dataStream = emitter;
+        });
+
+        dataPublisher = publisher.publish();
+        dataPublisher.connect();
     }
 
     public Publisher<SensorData> getGeneralPublisher() {
-        return sink.asFlux();
+        return dataPublisher;
     }
 
     public Publisher<SensorData> getContentFilteredPublisher(String content) {
-        return sink.asFlux()
+        return dataPublisher
                 .filter(gpsData -> gpsData.record()
                         .stream()
                         .map(RecordEntry::content)
@@ -32,15 +39,11 @@ public class GPSDataPublisher {
     }
 
     public Publisher<SensorData> getSinglePublisher(String id) {
-        return sink.asFlux()
+        return dataPublisher
                 .filter(gpsData -> gpsData.device().has(id));
     }
 
     public void publish(SensorData data) {
-        var result = sink.tryEmitNext(data);
-
-        if (result.isFailure()) {
-            throw new PublishErrorException("Failed to publish GPSData, reason: " + result.name());
-        }
+        dataStream.next(data);
     }
 }

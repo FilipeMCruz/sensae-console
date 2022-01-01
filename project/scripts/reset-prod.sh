4aeff781cdc9ba0d8f8762709692ac3e3479a214
FilipeMCruz <1170911@isep.ipp.pt>
2022-01-01 22:39:54 +0000

    Fixed bug in reset-prod.sh

--- project/scripts/reset-prod.sh
+++ project/scripts/reset-prod.sh
@@ -2,7 +2,7 @@
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-./project/scripts/validate-prod-config.sh
+"$ROOT_DIR"/project/scripts/validate-prod-config.sh
 if test ! "$?" -eq "0"; then
     echo "Check the documentation at project/README.md to define configurations for all containers"
     exit "$?"

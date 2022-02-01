68c9ab3e51d8393eb50767365d56f9f7f8f2d0ec
FilipeMCruz <1170911@isep.ipp.pt>
2022-02-01 17:09:21 +0000

    Updated data-validator with new version of iot-core

--- project/backend-services/sharespot-data-validator-backend/infrastructure/boot/src/main/java/sharespot/services/datavalidator/infrastructure/boot/config/AmqpConfiguration.java
+++ project/backend-services/sharespot-data-validator-backend/infrastructure/boot/src/main/java/sharespot/services/datavalidator/infrastructure/boot/config/AmqpConfiguration.java
@@ -8,9 +8,9 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pt.sharespot.iot.core.routing.keys.DataLegitimacyOptions;
-import pt.sharespot.iot.core.routing.keys.GPSDataOptions;
 import pt.sharespot.iot.core.routing.keys.InfoTypeOptions;
 import pt.sharespot.iot.core.routing.keys.RoutingKeysBuilderOptions;
+import pt.sharespot.iot.core.routing.keys.data.GPSDataOptions;
 import sharespot.services.datavalidator.application.RoutingKeysProvider;
 
 import static sharespot.services.datavalidator.infrastructure.boot.config.AmqpDeadLetterConfiguration.DEAD_LETTER_EXCHANGE;

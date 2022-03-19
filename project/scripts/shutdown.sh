496f2017615d0cce0ecf3e3889ea3db56c709656
FilipeMCruz <1170911@isep.ipp.pt>
2022-03-19 18:21:50 +0000

    Added decoder database creation script

--- project/scripts/shutdown.sh
+++ project/scripts/shutdown.sh
@@ -7,5 +7,4 @@ cd "$ROOT_DIR"/project || exit
 docker compose -f docker-compose.yml down
 docker compose -f docker-compose.dev.yml down
 docker compose -f docker-compose.backend.yml down
-docker compose -f docker-compose.local.yml down
 

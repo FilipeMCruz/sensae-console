2638e5538d23158855ee4edae35ee65b991430b8
FilipeMCruz <1170911@isep.ipp.pt>
2022-04-12 14:21:01 +0100

    Removed some junk

--- output/mutations/index.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports.createGarden = fs.readFileSync(path.join(__dirname, 'createGarden.gql'), 'utf8');
-module.exports.updateGarden = fs.readFileSync(path.join(__dirname, 'updateGarden.gql'), 'utf8');

84cae142d65d9ce4245ce4879030c7a2003b8d28
FilipeMCruz <1170911@isep.ipp.pt>
2022-05-16 17:22:49 +0100

    Added scripts to run dev env without docker

---
+++ project/scripts/compile-backend-dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+cd "$ROOT_DIR"/project/backend-services || exit
+
+rm --f -- ../../test-examples/backend-comp.log
+rm --f -- ../../test-examples/bakcend-fail.log
+
+ls | xargs -I % sh -c 'cd % && mvn clean install && echo % >> ../../test-examples/backend-comp.log || echo % >> ../../test-examples/backend-fail.log'

dad82a2a8c027f300c18f593e2d869532c587264
FilipeMCruz <1170911@isep.ipp.pt>
2022-05-26 13:48:48 +0100

    Added javax.xml.bind dependency

--- project/backend-services/smart-irrigation-backend/infrastructure/boot/src/main/java/pt/sensae/services/smart/irrigation/backend/infrastructure/boot/config/database/PostgresDataSourceConfiguration.java
+++ project/backend-services/smart-irrigation-backend/infrastructure/boot/src/main/java/pt/sensae/services/smart/irrigation/backend/infrastructure/boot/config/database/PostgresDataSourceConfiguration.java
@@ -10,13 +10,11 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jdbc.core.convert.DataAccessStrategy;
-import org.springframework.data.jdbc.core.convert.DefaultDataAccessStrategy;
-import org.springframework.data.jdbc.core.convert.JdbcConverter;
-import org.springframework.data.jdbc.core.convert.SqlGeneratorSource;
+import org.springframework.data.jdbc.core.convert.*;
 import org.springframework.data.jdbc.core.mapping.JdbcMappingContext;
 import org.springframework.data.jdbc.repository.config.DialectResolver;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.relational.core.dialect.Dialect;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -60,10 +58,14 @@ public class PostgresDataSourceConfiguration {
             JdbcConverter jdbcConverter,
             JdbcMappingContext context
     ) {
+        var dialect = DialectResolver.getDialect(operations.getJdbcOperations());
         return new DefaultDataAccessStrategy(
-                new SqlGeneratorSource(context, jdbcConverter,
-                        DialectResolver.getDialect(operations.getJdbcOperations())),
-                context, jdbcConverter, operations);
+                new SqlGeneratorSource(context, jdbcConverter, dialect),
+                context,
+                jdbcConverter,
+                operations,
+                new SqlParametersFactory(context, jdbcConverter, dialect),
+                new InsertStrategyFactory(operations, new BatchJdbcOperations(operations.getJdbcOperations()), dialect));
     }
 
     @Bean

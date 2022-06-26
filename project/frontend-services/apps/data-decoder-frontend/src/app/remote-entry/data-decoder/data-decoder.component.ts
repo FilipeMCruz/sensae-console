bd268ab238b60503c5fbebf4404eff9519aec6e7
FilipeMCruz <1170911@isep.ipp.pt>
2022-06-26 16:35:13 +0100

    Added monaco editor to data decoder

--- project/frontend-services/apps/data-decoder-frontend/src/app/remote-entry/data-decoder/data-decoder.component.ts
+++ project/frontend-services/apps/data-decoder-frontend/src/app/remote-entry/data-decoder/data-decoder.component.ts
@@ -1,12 +1,16 @@
-import {Component, EventEmitter, Input, OnChanges, Output,} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild,} from '@angular/core';
 import {DataDecoder, DataDecoderViewType} from "@frontend-services/data-decoder/model";
+import {first} from "rxjs/operators";
+import {MonacoEditorService} from "@frontend-services/mutual";
+
+declare let monaco: any;
 
 @Component({
   selector: 'frontend-services-data-decoder',
   templateUrl: './data-decoder.component.html',
   styleUrls: ['./data-decoder.component.scss'],
 })
-export class DataDecoderComponent implements OnChanges {
+export class DataDecoderComponent implements OnChanges, OnInit {
   @Input() entry!: DataDecoder;
   @Input() dataDecoderViewEntry!: DataDecoderViewType;
   @Input() canEdit = false;
@@ -16,10 +20,22 @@ export class DataDecoderComponent implements OnChanges {
   @Output() deleteDataDecoderEvent =
     new EventEmitter<DataDecoder>();
 
+  public _editor: any;
+  @ViewChild('editorContainer', {static: true})
+  _editorContainer!: ElementRef;
+
   dataDecoder = DataDecoder.empty();
 
   dataDecoderView = DataDecoderViewType.New;
   dataDecoderViewType = DataDecoderViewType;
+
+  constructor(public monacoEditorService: MonacoEditorService) {
+    this.monacoEditorService.load();
+  }
+
+  ngOnInit() {
+    this.initMonaco();
+  }
 
   ngOnChanges(): void {
     if (this.entry) {
@@ -44,5 +60,22 @@ export class DataDecoderComponent implements OnChanges {
   private resetView() {
     this.dataDecoderView = this.dataDecoderViewType.New;
     this.dataDecoder = DataDecoder.empty();
+  }
+
+  private initMonaco(): void {
+    if (!this.monacoEditorService.loaded) {
+      this.monacoEditorService.loadingFinished.pipe(first()).subscribe(() => {
+        this.initMonaco();
+        this._editor.onDidChangeModelContent(() => this.dataDecoder.script.content = this._editor.getValue())
+      });
+      return;
+    }
+
+    this._editor = monaco.editor.create(
+      this._editorContainer.nativeElement, {
+        value: this.dataDecoder.script.content,
+        language: 'javascript',
+        readOnly: this.dataDecoderView === this.dataDecoderViewType.Compare || !this.canEdit
+      });
   }
 }

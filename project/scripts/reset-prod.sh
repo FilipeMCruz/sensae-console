9bb26fa224ec67567142d3cda5e03e9c5cfb4b7f
FilipeMCruz <1170911@isep.ipp.pt>
2022-08-20 19:32:58 +0100

    Updated docs

--- project/scripts/reset-prod.sh
+++ project/scripts/reset-prod.sh
@@ -10,4 +10,23 @@ fi
 
 cd "$ROOT_DIR"/project || exit
 
-docker-compose up -d --build
+# seperate container builds to ensure that the host doesn't OOM
+docker compose build data-gateway
+docker compose build data-validator
+docker compose build data-decoder-flow
+docker compose build data-processor-flow
+docker compose build device-ownership-flow
+docker compose build device-management-flow
+docker compose build device-commander
+
+docker compose build device-management-frontend
+docker compose build data-processor-frontend
+docker compose build data-decoder-frontend
+docker compose build identity-management-frontend
+docker compose build smart-irrigation-frontend
+docker compose build fleet-management-frontend
+docker compose build rule-management-frontend
+docker compose build notification-management-frontend
+docker compose build ui-aggregator
+
+docker-compose up -d

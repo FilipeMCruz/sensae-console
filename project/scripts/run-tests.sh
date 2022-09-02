d4dd4beac1e7511cb8ce075ef705c2ad3d0a427d
FilipeMCruz <1170911@isep.ipp.pt>
2022-09-02 19:39:47 +0100

    Added fronetnd tests, fixed test pipeline and script, fixed backend tests some that every test runs

--- project/scripts/run-tests.sh
+++ project/scripts/run-tests.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/sh
+#!/bin/bash
+set -eo pipefail
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 cd "$ROOT_DIR"/project || exit
 
-sed  's|$SENSAE_ADMIN_EMAIL|test@sensae.pt|g' secrets/templates/dev/databases/identity-management-initdb.sql > \
+sed 's|$SENSAE_ADMIN_EMAIL|test@sensae.pt|g' secrets/templates/dev/databases/identity-management-initdb.sql > \
         databases/identity-management-database/identity-management-initdb.sql
 
 docker-compose -f docker-compose.build.yml build
@@ -19,3 +20,8 @@ ls -I data-relayer | xargs -I % sh -c 'cd % && mvn test && \
  echo % >> ../../reports/backend-test-fail.log'
 
 test ! -f ../reports/backend-test-fail.log
+
+cd ../frontend-services || exit
+
+npm install
+npm test-all

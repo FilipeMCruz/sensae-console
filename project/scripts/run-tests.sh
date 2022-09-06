831a71b6e49d64ad6152b74594ab427dd520d521
FilipeMCruz <1170911@isep.ipp.pt>
2022-09-06 19:55:23 +0100

    Test github action

--- project/scripts/run-tests.sh
+++ project/scripts/run-tests.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-set -eo pipefail
+sudo lsof -N -P -i | grep LISTEN  
+# set -eo pipefail
 
-ROOT_DIR=$(git rev-parse --show-toplevel)
+# ROOT_DIR=$(git rev-parse --show-toplevel)
 
-cd "$ROOT_DIR"/project || exit
+# cd "$ROOT_DIR"/project || exit
 
-./scripts/generate-test-config.sh "$@"
+# ./scripts/generate-test-config.sh "$@"
 
-docker-compose -f docker-compose.build.yml build
+# docker-compose -f docker-compose.build.yml build
 
-rm --f -- reports/backend-test-pass.log
-rm --f -- reports/backend-test-fail.log
+# rm --f -- reports/backend-test-pass.log
+# rm --f -- reports/backend-test-fail.log
 
-cd backend-services || exit
+# cd backend-services || exit
 
-ls -I data-relayer | xargs -I % sh -c 'cd % && mvn test && \
- echo % >> ../../reports/backend-test-pass.log || \
- echo % >> ../../reports/backend-test-fail.log'
+# ls -I data-relayer | xargs -I % sh -c 'cd % && mvn test && \
+#  echo % >> ../../reports/backend-test-pass.log || \
+#  echo % >> ../../reports/backend-test-fail.log'
 
-test ! -f ../reports/backend-test-fail.log
+# test ! -f ../reports/backend-test-fail.log
 
-cd ../frontend-services || exit
+# cd ../frontend-services || exit
 
-npm install
-npm run test-all
+# npm install
+# npm run test-all
 
-./../scripts/build-images.sh
+# ./../scripts/build-images.sh
 
-docker-compose -f ../docker-compose.test.yml up -d --build
+# docker-compose -f ../docker-compose.test.yml up -d --build
 
-sleep 60
+# sleep 60
 
-npm run e2e-all
+# npm run e2e-all
 
-docker-compose -f ../docker-compose.test.yml down
+# docker-compose -f ../docker-compose.test.yml down

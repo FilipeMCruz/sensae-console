82ab5cb528af902b7b9e43c59ee42753217f37b7
FilipeMCruz <1170911@isep.ipp.pt>
2022-09-06 20:03:54 +0100

    Test github action

--- project/secrets/test/frontend/data-decoder-frontend.ts
+++ project/secrets/test/frontend/data-decoder-frontend.ts
@@ -3,7 +3,7 @@ export const environment = {
   endpoints: {
     dataDecoder: {
       backend: {
-        domain: "localhost:8084",
+        domain: "localhost:8184",
         http: {
           scheme: "http://",
           path: "/graphql",

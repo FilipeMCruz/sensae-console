21f6d8d5bd25df19c3e37db96be143851274b023
FilipeMCruz <1170911@isep.ipp.pt>
2022-09-17 00:08:08 +0100

    Removed jmeter tests

--- project/jmeter-tests/scripts/new-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-for i in $(seq -f "%03g" 1 $1); do
-    echo \"P$i - Milesight EM300-TH\",\"$(cat /proc/sys/kernel/random/uuid)\",\"$(shuf -i 35-45 -n 1).$(shuf -i 000001-999999 -n 1)\",\"-$(shuf -i 5-10 -n 1).$(shuf -i 000001-999999 -n 1)\"
-done
-

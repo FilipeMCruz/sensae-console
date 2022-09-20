514cf1f2b54a6b83deb6820539c53a9bac6f227b
FilipeMCruz <1170911@isep.ipp.pt>
2022-09-20 10:21:50 +0100

    Fixed test environment

--- project/scripts/build-test-images.sh
+++ project/scripts/build-test-images.sh
@@ -5,25 +5,25 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 cd "$ROOT_DIR"/project || exit
 
 # seperate container builds to ensure that the host doesn't OOM
-docker-compose -f docker-compose.test.yml build data-gateway
-docker-compose -f docker-compose.test.yml build data-validator
-docker-compose -f docker-compose.test.yml build data-decoder-flow
-docker-compose -f docker-compose.test.yml build data-processor-flow
-docker-compose -f docker-compose.test.yml build device-ownership-flow
-docker-compose -f docker-compose.test.yml build device-management-flow
-docker-compose -f docker-compose.test.yml build device-commander
-docker-compose -f docker-compose.test.yml build data-store
+docker compose -f docker-compose.test.yml build data-gateway
+docker compose -f docker-compose.test.yml build data-validator
+docker compose -f docker-compose.test.yml build data-decoder-flow
+docker compose -f docker-compose.test.yml build data-processor-flow
+docker compose -f docker-compose.test.yml build device-ownership-flow
+docker compose -f docker-compose.test.yml build device-management-flow
+docker compose -f docker-compose.test.yml build device-commander
+docker compose -f docker-compose.test.yml build data-store
 
-docker-compose -f docker-compose.test.yml build smart-irrigation-backend 
-docker-compose -f docker-compose.test.yml build fleet-management-backend 
-docker-compose -f docker-compose.test.yml build notification-management-backend
-docker-compose -f docker-compose.test.yml build notification-dispatcher-backend
-docker-compose -f docker-compose.test.yml build alert-dispatcher-backend
-docker-compose -f docker-compose.test.yml build device-management-master-backend
-docker-compose -f docker-compose.test.yml build rule-management-backend
-docker-compose -f docker-compose.test.yml build identity-management-backend
-docker-compose -f docker-compose.test.yml build data-decoder-master-backend
-docker-compose -f docker-compose.test.yml build data-processor-master-backend
+docker compose -f docker-compose.test.yml build smart-irrigation-backend 
+docker compose -f docker-compose.test.yml build fleet-management-backend 
+docker compose -f docker-compose.test.yml build notification-management-backend
+docker compose -f docker-compose.test.yml build notification-dispatcher-backend
+docker compose -f docker-compose.test.yml build alert-dispatcher-backend
+docker compose -f docker-compose.test.yml build device-management-master-backend
+docker compose -f docker-compose.test.yml build rule-management-backend
+docker compose -f docker-compose.test.yml build identity-management-backend
+docker compose -f docker-compose.test.yml build data-decoder-master-backend
+docker compose -f docker-compose.test.yml build data-processor-master-backend
 
 docker compose -f docker-compose.test.yml build device-management-frontend
 docker compose -f docker-compose.test.yml build data-processor-frontend
